e4fc7cb638bc484b1c30f4c283cd2a5a0c2c0208
yuxinfeng <yuxinfeng0806@gmail.com>
2014-08-06 18:16:59 +0800

    text: upload_file

--- app.js
+++ app.js
@@ -6,39 +6,28 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var config = require('./config').config;
-var routes = require('./routes');
+var routes = require('./routes/basic.js');
 var app = express();
 var morgan = require('morgan');
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-
 app.set('view engine', 'ejs');
 app.engine('ejs', engine);
-
-app.engine('html', require('ejs').renderFile);
 
 app.use(favicon());
 app.use(morgan('dev')); // logger模块
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(cookieSession({
-  secret: config.session_secret,
-  cookie: {
-    maxage: config.max_age
-  }
-}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-routes(app);
-
+app.use(routes);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 /// error handlers
@@ -46,11 +35,11 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+    app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-      message: err.message,
-      error: err
+        message: err.message,
+        error: err
     });
   });
 }
@@ -58,11 +47,11 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 app.listen(config.port);
--- routes/basic.js
+++ routes/basic.js
@@ -2,11 +2,6 @@ var express = require('express');
 var router = express.Router();
 var registerModule = require('./register_avatar.js');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-    res.render('index', { title: 'Express' });
-});
-
 /*upload Personal Image*/
 router.get('/uploadownimage',registerModule.uploadOwnImage);
 router.post('/uploadownimage',registerModule.postImage);
