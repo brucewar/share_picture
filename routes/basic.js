c3b9a3b7e7baf708d1b5d6c27b93be0df52aa5b1
brucewar <wjl891014@gmail.com>
2014-08-06 22:50:39 +0800

    reconstruct content

--- routes/basic.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var registerModule = require('./register_avatar.js');
-
-/*upload Personal Image*/
-router.get('/uploadownimage',registerModule.uploadOwnImage);
-router.post('/uploadownimage',registerModule.postImage);
-
-module.exports = router;
