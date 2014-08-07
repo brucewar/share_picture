710fd780314ab83065866fcf57d39d40f37af573
yuxinfeng <yuxinfeng0806@gmail.com>
2014-08-07 17:49:08 +0800

    text:upload user avatar

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,8 @@
-exports.show = function(req, res, next) {
-	res.render('user/index', {name: 'brucewar'});
-}
+exports.registerUser = function(req, res, next) {
+  res.render('user/register');
+};
+exports.saveavatarImage = function(req, res, next) {
+	console.log(req.body);
+	console.log(1);
+
+} ;
--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var user = require('./controllers/user');
 
-router.get('/', user.show);
+router.get('/register', user.registerUser);
+router.post('/uploadownimage', user.saveavatarImage);
 
 module.exports = router;
