e7e76686667f1f1a79fc80e33c14868103ceb24f
yuxinfeng <yuxinfeng@gmail.com>
2014-08-11 18:45:23 +0800

    text: ui avatar

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-/**
+ /**
  * config of share_picture
  */
 
--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,3 @@
-var mysql = require('mysql');
-var images = require('images');
-exports.update = function(req, res, next) {
-  var userId = req.params.userId
-	if(!userId || '' == userId){
-    res.render('user/index');
-  }else{
-
-  }
+exports.registerUser = function(req, res, next) {
+  res.render('user/register');
 };
